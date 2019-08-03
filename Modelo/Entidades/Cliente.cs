a5f2cefa0576cae68f6464be291a1ddceb424638
Lucas <lucas1932003@hotmail.com>
2019-08-02 22:24:55 -0300

    Inicio de entidad Usuario, Entrega y Cliente

---
+++ Modelo/Entidades/Cliente.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modelo.Entidades
+{
+    public class Cliente
+    {
+    }
+}
---
+++ Modelo/Entidades/Reportes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modelo.Entidades
+{
+    public class Reportes
+    {
+
+    }
+}
